e8984df92b5be15649774ecb8b3f67fedfd8d8de
farzonashukurova <farash19@gmail.com>
2022-04-08 22:33:28 -0400

    created Test 1

--- src/mainClass.java
+++ src/mainClass.java
@@ -1,5 +1,29 @@
 public class mainClass {
     public static void main(String[] args) {
+        Test();
+        Test2();
+        System.out.println(Test());
+        System.out.println(Test2());
 
     }
+    public static void Test(){
+        int a = 0;
+        int b = 0;
+        int sum = 0;
+
+        a = 7;
+        b = 5;
+        sum = a + b;
+
+    }
+    public static int Test2 (){
+        int a = 0;
+        int b = 0;
+        int sum = 0;
+
+        a = 7;
+        b = 5;
+        sum = a + b;
+    }
 }
+
